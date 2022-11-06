09c464408561e5ddea72aa3f169472abb4c6de21
JoaoVanzuita <joaog17vanzuita@gmail.com>
2022-11-06 11:56:28 -0300

    Client crud implementation

--- src/main/java/com/joaovanzuita/deliveryapi/controller/ClientController.java
+++ src/main/java/com/joaovanzuita/deliveryapi/controller/ClientController.java
@@ -1,35 +1,80 @@
 package com.joaovanzuita.deliveryapi.controller;
 
-
 import com.joaovanzuita.deliveryapi.domain.model.Client;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.joaovanzuita.deliveryapi.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
-    @GetMapping("/clients")
+//    @Autowired
+    private ClientRepository clientRepository;
+
+    public ClientController(ClientRepository clientRepository) {
+        super();
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping
     public List<Client> list(){
 
-        Client client1 = new Client();
-
-        client1.setEmail("email");
-        client1.setName("name");
-        client1.setPhone("phone");
-
-        Client client2 = new Client();
-
-        client2.setEmail("email");
-        client2.setName("name");
-        client2.setPhone("phone");
-
-        ArrayList<Client> clients = new ArrayList<>();
-        clients.add(client1);
-        clients.add(client2);
+        List<Client> clients = clientRepository.findAll();
 
         return clients;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id){
+
+        return clientRepository.findById(id)
+                .map(client -> ResponseEntity.ok(client))
+                .orElse(ResponseEntity.notFound().build());
+
+//        Optional<Client> client = clientRepository.findById(id);
+//
+//        if(client.isPresent()){
+//           return ResponseEntity.ok(client.get());
+//        }
+//
+//        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client client){
+
+        return clientRepository.save(client);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> update(@RequestBody Client client, @PathVariable Long id){
+
+        if(!clientRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        client.setId(id);
+        client = clientRepository.save(client);
+
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+
+        if (!clientRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        clientRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/joaovanzuita/deliveryapi/domain/model/Client.java
+++ src/main/java/com/joaovanzuita/deliveryapi/domain/model/Client.java
@@ -1,18 +1,29 @@
 package com.joaovanzuita.deliveryapi.domain.model;
 
+import javax.persistence.*;
+import java.util.Objects;
 
+@Entity
+@Table(name = "clients")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String phone;
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -37,5 +48,18 @@ public class Client {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id.equals(client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
